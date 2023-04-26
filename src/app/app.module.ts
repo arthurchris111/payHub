2f706d1af2a2956ad31aecef478b19f776a633ff
christian <arthurchristian111@gmail.com>
2023-04-26 16:35:33 +0000

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { CoreComponent } from './core/core.component';
+import { LayoutComponent } from './layout/layout.component';
+import { ModulesComponent } from './modules/modules.component';
+import { LoginComponent } from './modules/authentication/partials/login/login.component';
+import { SignupComponent } from './modules/authentication/partials/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    CoreComponent,
+    LayoutComponent,
+    ModulesComponent,
+    LoginComponent,
+    SignupComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
