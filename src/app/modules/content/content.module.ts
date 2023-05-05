57af00bde47e176d2a34acca07d3c0e8aaa8b045
christian <arthurchristian111@gmail.com>
2023-05-05 23:11:23 +0000

    login

--- src/app/modules/content/content.module.ts
+++ src/app/modules/content/content.module.ts
@@ -2,11 +2,11 @@ import { ContentComponent } from './content.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-
 import { ContentRoutingModule } from './content-routing.module';
+import { LoginModule } from '../authentication/partials/login/login.module';
 
 @NgModule({
   declarations: [ContentComponent, HomeComponent],
-  imports: [CommonModule, ContentRoutingModule],
+  imports: [CommonModule, ContentRoutingModule, LoginModule],
 })
 export class ContentModule {}
--- src/app/modules/content/home/home.component.ts
+++ src/app/modules/content/home/home.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ReadMore: boolean = false;
+
+  //hiding info box
+  visible: boolean = true;
+
+  //onclick toggling both
+  onclick() {
+    this.ReadMore = !this.ReadMore; //not equal to condition
+    this.visible = !this.visible;
   }
-
 }
