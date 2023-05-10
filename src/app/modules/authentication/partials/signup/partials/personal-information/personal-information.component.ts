fa2cb8a3dd55603f174703278819951ab3c650c3
christian <arthurchristian111@gmail.com>
2023-05-10 22:45:02 +0000

    added personal information form

--- src/app/modules/authentication/partials/signup/partials/personal-information/personal-information.component.ts
+++ src/app/modules/authentication/partials/signup/partials/personal-information/personal-information.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-information',
   templateUrl: './personal-information.component.html',
-  styleUrls: ['./personal-information.component.scss']
+  styleUrls: ['./personal-information.component.scss'],
 })
 export class PersonalInformationComponent implements OnInit {
+  signup!: FormGroup;
+  submitted: boolean = false;
+  show: boolean = false;
+  user: any = {};
+  userSubmitted: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private route: Router) {}
 
-  ngOnInit(): void {
+  signupForm(): void {
+    this.signup = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.minLength(10)]],
+    });
   }
 
+  ngOnInit(): void {
+    this.signup;
+    this.signupForm();
+  }
+  ReadMore: boolean = false;
+
+  //hiding info box
+  visible: boolean = true;
+
+  //onclick toggling both
+  onclick() {
+    this.ReadMore = !this.ReadMore; //not equal to condition
+    this.visible = !this.visible;
+  }
 }
--- src/app/modules/authentication/partials/signup/signup.component.ts
+++ src/app/modules/authentication/partials/signup/signup.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -9,34 +6,7 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-  signup!: FormGroup;
-  submitted: boolean = false;
-  show: boolean = false;
-  user: any = {};
-  userSubmitted: boolean = false;
+  ngOnInit(): void {}
 
-  constructor(private formBuilder: FormBuilder, private route: Router) {}
-
-  signupForm(): void {
-    this.signup = this.formBuilder.group({
-      firstName: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      phone: ['', [Validators.required, Validators.minLength(10)]],
-    });
-  }
-
-  ngOnInit(): void {
-    this.signup;
-    this.signupForm();
-  }
-  ReadMore: boolean = false;
-
-  //hiding info box
-  visible: boolean = true;
-
-  //onclick toggling both
-  onclick() {
-    this.ReadMore = !this.ReadMore; //not equal to condition
-    this.visible = !this.visible;
-  }
+  constructor() {}
 }
