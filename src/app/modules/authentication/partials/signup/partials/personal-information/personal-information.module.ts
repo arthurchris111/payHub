7bafd6a122c15719772611e1c74faf6f29656669
christian <arthurchristian111@gmail.com>
2023-05-11 21:39:32 +0000

    signup

--- src/app/modules/authentication/partials/signup/partials/personal-information/personal-information.module.ts
+++ src/app/modules/authentication/partials/signup/partials/personal-information/personal-information.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonalInformationComponent } from './personal-information.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [PersonalInformationComponent],
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterModule],
   exports: [PersonalInformationComponent],
 })
 export class PersonalInformationModule {}
--- src/app/modules/authentication/partials/signup/signup.module.ts
+++ src/app/modules/authentication/partials/signup/signup.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AddressInformationComponent } from './partials/address-information/address-information.component';
-import { IdentityCardInformationComponent } from './partials/identity-card-information/identity-card-information.component';
 
 @NgModule({
-  declarations: [
-    SignupComponent,
-    AddressInformationComponent,
-    IdentityCardInformationComponent,
-  ],
+  declarations: [SignupComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [SignupComponent],
 })
