46d9ba27c542d656210ca5fdf649da5973758374
christian <arthurchristian111@gmail.com>
2023-05-15 18:25:02 +0000

    added all the regions in an array

--- src/app/modules/authentication/partials/signup/partials/address-information/address-information.component.ts
+++ src/app/modules/authentication/partials/signup/partials/address-information/address-information.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-address-information',
   templateUrl: './address-information.component.html',
-  styleUrls: ['./address-information.component.scss']
+  styleUrls: ['./address-information.component.scss'],
 })
 export class AddressInformationComponent implements OnInit {
+  address!: FormGroup;
+  submitted: boolean = false;
+  show: boolean = false;
+  user: any = {};
+  userSubmitted: boolean = false;
+  isSubmitted: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private route: Router) {}
 
-  ngOnInit(): void {
+  regions: any = [
+    'Northern',
+    'Ashanti',
+    'Western',
+    'Volta',
+    'Eastern',
+    'Upper West',
+    'Central',
+    'Upper East',
+    'Greater Accra',
+    'Savannah',
+    'North East',
+    'Bono East',
+    'Oti',
+    'Ahafo',
+    'Bono',
+    'Western North',
+  ];
+
+  addressForm(): void {
+    this.address = this.formBuilder.group({
+      region: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      city: ['', [Validators.required, Validators.minLength(10)]],
+    });
   }
 
+  ngOnInit(): void {
+    this.address;
+    this.addressForm();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.userSubmitted = true;
+
+    if (this.address.invalid) {
+      return;
+    }
+  }
 }
