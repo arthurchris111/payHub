771dc861f14b5a68eb59552fa24ade2036f7ddb8
christian <arthurchristian111@gmail.com>
2023-05-15 23:30:28 +0000

    select id type added

--- src/app/modules/authentication/partials/signup/partials/identity-card-information/identity-card-information.component.ts
+++ src/app/modules/authentication/partials/signup/partials/identity-card-information/identity-card-information.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-identity-card-information',
   templateUrl: './identity-card-information.component.html',
-  styleUrls: ['./identity-card-information.component.scss']
+  styleUrls: ['./identity-card-information.component.scss'],
 })
 export class IdentityCardInformationComponent implements OnInit {
+  identityCard!: FormGroup;
+  submitted: boolean = false;
+  show: boolean = false;
+  user: any = {};
+  userSubmitted: boolean = false;
+  isSubmitted: boolean = false;
+  cardName: any;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private route: Router) {}
 
-  ngOnInit(): void {
+  Cards: any = ['Voters ID', 'Ghana card'];
+
+  cardForm(): void {
+    this.identityCard = this.formBuilder.group({
+      card: ['', [Validators.required]],
+      // address: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {
+    this.identityCard;
+    this.cardForm();
+  }
+
+  changeCard(e: any) {
+    this.cardName?.setValue(e.target.value, {});
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.userSubmitted = true;
+    this.isSubmitted = true;
+
+    if (this.identityCard.invalid) {
+      return;
+    }
+  }
 }
