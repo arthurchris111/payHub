06787a72d2d66d272fffe865a6de21f88ef042a8
christian <arthurchristian111@gmail.com>
2023-06-01 22:03:43 +0000

    added input component

--- src/app/modules/authentication/partials/signup/partials/personal-information/personal-information.component.ts
+++ src/app/modules/authentication/partials/signup/partials/personal-information/personal-information.component.ts
@@ -32,7 +32,7 @@ export class PersonalInformationComponent implements OnInit {
 
   nextSection() {
     this.submitted = true;
-    // if (this.personalForm.invalid) return;
+    if (this.personalForm.invalid) return;
     // Track data
     this.next.emit(this.personalForm);
   }
