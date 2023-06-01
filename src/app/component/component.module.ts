8c2260c99d6c55883992bcd4af45382c32be03d0
christian <arthurchristian111@gmail.com>
2023-06-01 23:56:24 +0000

    button component

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { RouterModule } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent, ButtonComponent],
   imports: [CommonModule, RouterModule],
   exports: [InputComponent],
 })
