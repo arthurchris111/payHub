272f98a86c64929dc87375cace8700b4d6290b1d
Christian <arthurchristian111@gmail.com>
2023-10-13 00:23:15 +0000

    .

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutModule } from '../layout/layout.module';
 import { HomeComponent } from './home/home.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, FaqComponent],
   imports: [CommonModule, PagesRoutingModule, LayoutModule],
 })
 export class PagesModule {}
