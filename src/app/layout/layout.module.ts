555f8e0648fd0610c86055935e0f66547a01b779
Christian <arthurchristian111@gmail.com>
2023-10-13 22:05:57 +0000

    changes

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
-import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
