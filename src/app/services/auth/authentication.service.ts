7680f3c15edc129bf96a48040d728225c19b9eea
Christian <arthurchristian111@gmail.com>
2023-11-12 23:51:51 +0000

    ,

--- src/app/services/auth/authentication.service.ts
+++ src/app/services/auth/authentication.service.ts
@@ -14,6 +14,7 @@ import { Observable, catchError, from, throwError } from 'rxjs';
 })
 export class AuthenticationService {
   // currentUser = authState(this.auth);
+  // currentUser = authState(this.auth);
 
   constructor(private auth: Auth) {}
 
